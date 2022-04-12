8f5d51025daf9710c11b437a8bc57fc71d686707
Assumptaginika <ozoudeassumpta@gmail.com>
2022-04-12 13:11:56 -0700

    0-putchar.c

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,12 +1,8 @@
-#include <stdio.h>
+#include <stdio.c>
 
 int main(void)
 {
-char p[8] = {'_','p',...,'r'};
-int i;
-for (i = 0; i < 8, i++);
-{
-putchar(p[i]);
-}
-putchar('\n');
+puts('_putchar');
+puts("\n");
 return 0;
+}
