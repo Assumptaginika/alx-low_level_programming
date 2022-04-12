d55913073873a2d2bacc53b9a827d0f672e97a0c
Assumptaginika <ozoudeassumpta@gmail.com>
2022-04-12 07:17:16 -0700

    0-putchar.c

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,23 +1,18 @@
 #include "main.h"
 /**
- * main - prints a string
- * Description: the string is _putchar
- *
+ * main - entry block
+ * @void: no argument
  * Return: 0
- */
+ **/
 int main(void)
 {
-	int str[] = {95, 112, 117, 116, 99, 104, 97, 114};
-	int count, sz;
-
-	sz = sizeof(str) / sizeof(int);
-	for (count = 0; count < sz; count++)
-
+	char ermaias[] = "_putchar";
+	int c;
+	for (c = 0; c < 8; c++)
 	{
-		_putchar(str[count]);
-
+		_putchar(ermias[c]);
 	}
-
 	_putchar('\n');
 	return (0);
 }
+
