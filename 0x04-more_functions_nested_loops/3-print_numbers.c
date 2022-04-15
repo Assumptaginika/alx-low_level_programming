98b50d2059ac5187fd0609cca2b3cdad236485e4
Assumptaginika <ozoudeassumpta@gmail.com>
2022-04-15 14:52:30 -0700

    really hard

--- 0x04-more_functions_nested_loops/3-print_numbers.c
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -1,15 +1,22 @@
 #include "main.h"
 
 /**
- * print_numbers - a function that prints the numbers, from 0 to 9
+ * main - print_numbers - a function that prints the numbers, from 0 to 9
  *
- * Return: 0-9 followed by newline
+ * Return: Always 0.
  */
-void print_numbers(void)
+int main(void)
+	void print_numbers(void)
 {
-	int i;
+    int i;
 
-	for (i = 0; i <= 9; i++)
-		_putchar(i + '0');
-	_putchar('\n');
+    i = 0;
+    while (i < 10)
+    {
+	    _putchar (i + '0');
+	    i++;
+    }
+
+    _putchar ('\n');
+
 }
--- 0x04-more_functions_nested_loops/main.h
+++ 0x04-more_functions_nested_loops/main.h
@@ -1,8 +1,10 @@
-#ifndef MAIN_H
-#define MAIN_H
+# ifndef MAIN_H
+# define MAIN_H
 
 #include <stdio.h>
 
+int _putchar(char.c);
 void print_numbers(void);
 
 #endif
+
